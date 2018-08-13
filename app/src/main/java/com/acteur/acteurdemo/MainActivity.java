c70a3bd5ee0c1b8f75c6ee381dc2c2c653815d76
Acteur1116 <1156137954@qq.com>
2018-08-13 20:31:21 +0800

    zz

---
+++ app/src/main/java/com/acteur/acteurdemo/MainActivity.java
@@ -0,0 +1,16 @@
+package com.acteur.acteurdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import retrofit2.http.POST;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
